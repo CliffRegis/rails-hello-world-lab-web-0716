c380f5fe5928fdcb794df4d1497b2ca63fec90ef
Clifton Regis <github email address>
2016-08-16 22:10:20 -0400

    route page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+
+	def hello_world
+
+	  render "static/hello_world"
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # You can have the root of your site routed with "roo
+  get '/hello_world', to: 'welcome#hello_world'
 
+#   get '/hello_worldindex' do
+
+#   erb :index
+# end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
